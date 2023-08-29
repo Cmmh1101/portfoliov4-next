ce279ebc665581ddb5cab31b51741c7f8035a87f
Carla Montano <cm.emprendiendo@gmail.com>
2023-08-28 22:40:33 -0500

    removed cypress

--- components/home/HomeComponent.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import HomeComponent from './HomeComponent'
-
-describe('<HomeComponent />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<HomeComponent />)
-  })
-})
--- context/ThemeModeContextThemeModeProvider.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { ThemeModeProvider } from './ThemeModeContext'
-
-describe('<ThemeModeProvider />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<ThemeModeProvider />)
-  })
-})
