9d4dd5c21c445c0ccb7db6ef3692ea2a77fd31d2
Carla Montano <cm.emprendiendo@gmail.com>
2023-09-14 22:28:55 -0500

    add logo

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -2,12 +2,14 @@ import { useTheme } from "@/components/context/ThemeModeContext";
 import Link from "next/link";
 import React from "react";
 import NavLinks from "./NavLinks";
+import Image from "next/image";
+import logo from "../../public/logoblue.png"
 
 const Navbar = () => {
   const { handleToggleTheme, darkMode } = useTheme();
   return (
-    <div className="w-full container flex justify-between items-center h-10 mx-auto">
-          <Link href="/">CMLogo</Link>
+    <div className="w-full container flex justify-between items-center h-20 mx-auto">
+          <Link href="/" className="w-52 h-auto"><Image src={logo} width={200} height={200} alt="Logo" style={{objectFit: "cover", objectPosition:"center"}} /></Link>
           <NavLinks />
       <button onClick={handleToggleTheme}>
         {darkMode ? "Light" : "Dark"}
