d085c1afda3a7953e590968c90b9f9a0cf5c091d
Carla Montano <cm.emprendiendo@gmail.com>
2023-09-15 00:01:30 -0500

    initial nav styles

--- components/layout/NavLinks.tsx
+++ components/layout/NavLinks.tsx
@@ -1,47 +1,53 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 interface LinkProps {
-    url: string;
-    title: string
+  url: string;
+  title: string;
 }
 
 const links = [
-    {
-        url: "/",
-        title: "home"
-    },
-    {
-        url: "/projects",
-        title: "projects"
-    },{
-        url: "/about",
-        title: "about"
-    },{
-        url: "/experience",
-        title: "experience"
-    },
-    {
-        url: "/connect",
-        title: "connect"
-    },
-    {
-        url: "/blog",
-        title: "blog"
-    },
-]
+  {
+    url: "/",
+    title: "home",
+  },
+  {
+    url: "/projects",
+    title: "projects",
+  },
+  {
+    url: "/about",
+    title: "about",
+  },
+  {
+    url: "/experience",
+    title: "experience",
+  },
+  {
+    url: "/connect",
+    title: "connect",
+  },
+  {
+    url: "/blog",
+    title: "blog",
+  },
+];
 
-const NavLinks = () => {
+const NavLinks: React.FC<{openNav: Boolean, handleToggleNav: () => void}> = ({openNav, handleToggleNav}) => {
+    
+    
+    const hideNav = openNav ? "translate-y-24" : "-translate-y-[1000px]"
   return (
-      <nav className='flex justify-around capitalize'>
-          {links.map((link, i) => {
-              return (
-                  <Link href={link.url} key={i} className='mx-4'>{link.title}</Link>
-              )
-          })}
-          
-      </nav>
-  )
-}
+    <nav className={`flex flex-col lg:flex-row justify-around lg:justify-center py-48 lg:py-0 transform capitalize z-10 absolute -top-10 left-0 lg:top-0 lg:h-auto h-screen lg:relative lg:mx-3 text-center bg-white text-black w-full lg:translate-y-0 ${hideNav}`}>
+      {links.map((link, i) => {
+        return (
+          <Link href={link.url} key={i} className="mx-4 text-2xl lg:text-xl" onClick={handleToggleNav}>
+            {link.title}
+          </Link>
+        );
+      })}
+    </nav>
+  );
+};
 
-export default NavLinks
+export default NavLinks;
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,19 +1,33 @@
 import { useTheme } from "@/components/context/ThemeModeContext";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import NavLinks from "./NavLinks";
 import Image from "next/image";
 import logo from "../../public/logoblue.png"
+import { AiOutlineClose } from "react-icons/ai"
+import {RxHamburgerMenu} from "react-icons/rx"
+import { MdDarkMode, MdLightMode } from "react-icons/md";
 
 const Navbar = () => {
+  const [mobile, setMobile] = useState(false)
+  const [openNav, setOpenNav] = useState(false)
   const { handleToggleTheme, darkMode } = useTheme();
+
+  const handleToggleNav = () => {
+    setOpenNav(!openNav)
+  }
   return (
-    <div className="w-full container flex justify-between items-center h-20 mx-auto">
-          <Link href="/" className="w-52 h-auto"><Image src={logo} width={200} height={200} alt="Logo" style={{objectFit: "cover", objectPosition:"center"}} /></Link>
-          <NavLinks />
+    <div className="w-full container flex justify-between items-center h-20 mx-auto shadow-b-xl shadow-b-black z-20">
+      <Link href="/" className="w-52 h-auto z-50"><Image src={logo} width={200} height={200} alt="Logo" style={{ objectFit: "cover", objectPosition: "center" }} priority /></Link>
+      <NavLinks openNav={openNav} handleToggleNav={handleToggleNav} />
       <button onClick={handleToggleTheme}>
-        {darkMode ? "Light" : "Dark"}
+        {darkMode ? <MdLightMode /> : <MdDarkMode />}
       </button>
+      <button className="lg:hidden" onClick={handleToggleNav}>
+      
+        {openNav ? <AiOutlineClose /> : <RxHamburgerMenu />}
+      </button>
+     
     </div>
   );
 };
