59284d0f9e1b286b2d3d42aca961d4cbead46fde
Carla Montano <cm.emprendiendo@gmail.com>
2023-09-20 22:28:07 -0500

    responsive experience

--- components/experience/ExperienceItem.tsx
+++ components/experience/ExperienceItem.tsx
@@ -20,17 +20,16 @@ const ExperienceItem: React.FC<{ experience: ExperienceItemProps }> = ({ experie
   return (
       <div>
           <div className='flex items-center'>
-              {experience?.image && <Image src={experience?.image!} alt='' width={100} height={100} className='mr-3 rounded-md' style={{objectFit: 'cover', objectPosition: 'center', width: "auto", height: "auto"}} />}
-              
-              <h2 className='text-3xl'>{experience?.title}</h2>
+              {experience?.image && <Image src={experience?.image!} alt='' width={100} height={100} className='mr-3 rounded-md hidden md:block' style={{objectFit: 'cover', objectPosition: 'center', width: "auto", height: "auto"}} />}
+              <h2 className=' text-2xl md:text-3xl'>{experience?.title}</h2>
           </div>
-          <p className='my-3 text-xl font-semibold'> { experience?.startDate} - { experience?.endDate}</p>
-          <p className='my-3 text-xl '>Location: {experience?.location}</p>
-          <p className='my-5 text-xl '>Role Description:</p>
+          <p className='my-3 text-lg md:text-xl font-semibold'> { experience?.startDate} - { experience?.endDate}</p>
+          <p className='my-3 text-lg md:text-xl'>Location: {experience?.location}</p>
+          <p className='my-5 text-lg md:text-xl'>Role Description:</p>
           <ul>
               {descriptionArray?.map((description, i) => {
                   return (
-                      <li className='my-5 text-md md:text-lg lg:text-xl' key={i}>{ description !== "" && <><SiCheckmarx className='float-left mr-2' /> {description}</>}</li>
+                      <li className='my-5 text-md md:text-lg lg:text-xl' key={i}>{ description !== "" && <><SiCheckmarx className='float-left mr-2 text-blue-400' /> {description}</>}</li>
               )
           })}
           </ul>
