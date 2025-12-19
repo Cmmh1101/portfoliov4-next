6d9749801ed2375c61f540c0bb279238508307b1
Carla Montano <cm.emprendiendo@gmail.com>
2025-12-18 23:14:27 -0400

    update to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Optional: if you're not using image optimization
+  },
 }
 
 module.exports = nextConfig
