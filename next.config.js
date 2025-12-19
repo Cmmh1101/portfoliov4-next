5a767fc17a5b3bbe8d58ce500380a82ae9e88094
Carla Montano <cm.emprendiendo@gmail.com>
2025-12-18 23:19:05 -0400

    add image optimizations

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true, // Optional: if you're not using image optimization
-  },
 }
 
 module.exports = nextConfig
